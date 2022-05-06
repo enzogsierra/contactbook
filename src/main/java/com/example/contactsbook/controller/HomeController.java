3f20835453314c28fbd8055f5ace3d4f13944634
Enzo Sierra <enzogsierra@gmail.com>
2022-05-06 18:22:39 -0300

    contacts are loaded in home page

--- src/main/java/com/example/contactsbook/controller/HomeController.java
+++ src/main/java/com/example/contactsbook/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.contactsbook.controller;
 
+import com.example.contactsbook.service.IContactService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController 
 {
+    @Autowired
+    private IContactService contactService;
+
+
     @GetMapping(value = {"", "/"})
     public String index(Model model)
     {
+        model.addAttribute("contacts", contactService.all());
         return "index";
     }    
 }
